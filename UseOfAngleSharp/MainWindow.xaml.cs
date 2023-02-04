afcbdfef2a37fa8a2daacaf075f614533bf15f03
FinT08 <IppolitovIS@gmail.com>
2023-02-04 23:49:29 +0300

    Добавьте файлы проекта.

---
+++ UseOfAngleSharp/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using libToGetVacancies;
+using System.Collections.Concurrent;
+using System.Reflection.Metadata;
+using System.Security.Policy;
+using System.Diagnostics;
+
+namespace UseOfAngleSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private async void buttonGetVac_Click(object sender, RoutedEventArgs e)
+        {
+            pbStatus.IsIndeterminate = true;
+
+            // Очистка существующего списка
+            dataGridVac.ItemsSource = null;
+            dataGridVac.Items.Clear();
+            dataGridVac.Items.Refresh();
+
+            // Главная страница
+            var uriMainPage = "https://proglib.io/vacancies/all";
+
+            // проверка на корректность строки адреса
+            if (!Uri.IsWellFormedUriString(uriMainPage, UriKind.Absolute))
+            {
+                MessageBox.Show("Строка запроса не соответствует адресу!");
+                pbStatus.IsIndeterminate = false;
+                return;
+            }
+
+            // Определение количества страниц
+            var numberOfPages = await FuncToGetVacancies.getNumberOfPages(uriMainPage);
+
+            if (numberOfPages.Item1 == false)
+            {
+                MessageBox.Show("Ошибка определения колчиества страниц!");
+                pbStatus.IsIndeterminate = false;
+                return;
+            }
+
+            // Инициализация массива с необходимым количеством элементов
+            var arrOfPages = Enumerable.Range(0, numberOfPages.Item2).ToArray();
+
+            // ConcurrentBag использовал для проерки
+            // Результаты совпали, поэтому закоментировал
+            var dataBag = new ConcurrentBag<VacancyData>();
+
+            // Общая часть названия страницы
+            var uri = "https://proglib.io/vacancies/all?workType=all&workPlace=all&experience=&salaryFrom=&page=";
+
+            // Определение макимального количество параллельных потоков
+            ParallelOptions parallelOptions = new()
+            {
+                MaxDegreeOfParallelism = 5
+            };
+
+            //Считывание необходимых данных с сайта параллельно
+            await Parallel.ForEachAsync(arrOfPages, parallelOptions, async (i, _) =>
+            {
+                await FuncToGetVacancies.fillVacanciesFromPage(dataBag, uri + (i + 1));
+            });
+
+            dataGridVac.ItemsSource = dataBag;
+
+            pbStatus.IsIndeterminate = false;
+        }
+        private void DG_Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Hyperlink link = (Hyperlink)e.OriginalSource;
+
+            var psi = new ProcessStartInfo
+            {
+                FileName = link.NavigateUri.AbsoluteUri,
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+    }
+}
---
+++ libToGetVacancies/LibToWorkWithVac.cs
@@ -0,0 +1,95 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using System.Collections.Concurrent;
+using System.Xml.Linq;
+
+namespace libToGetVacancies
+{
+
+    // Класс для идентификации контакта
+    public class VacancyData
+    {
+        public string Vacancy { get; set; }
+        public Uri Link { get; set; }
+        public string Date { get; set; }
+
+        public VacancyData(string vacancy, string link, string date) {
+            Vacancy = vacancy;
+            Link = new System.Uri(link);
+            Date = date;
+        }
+    }
+
+    public class FuncToGetVacancies
+    {
+        public static async Task<(bool, int)> getNumberOfPages(string mainPage, 
+                                                string selector = "div > div > main > div.feed-pagination.flex.align-center", 
+                                                string attributeName = "data-total")
+        {
+
+            int numberOfPages;
+
+            // проверка на возможность возврата значения
+            try
+            {
+                IConfiguration config = Configuration.Default.WithDefaultLoader();
+                IBrowsingContext context = BrowsingContext.New(config);
+                IDocument document = await context.OpenAsync(mainPage);
+
+                // Проверка успешного запроса
+                if (document.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    return (false, 0);
+                }
+
+                // Парсинг с проверкой результата
+                if (!int.TryParse(document.QuerySelector(selector).GetAttribute(attributeName), out numberOfPages))
+                {
+                    return (false, 0);
+                }
+            }
+            catch (Exception)
+            {
+                return (false, 0);
+            }
+            return (true, numberOfPages);
+        }
+
+        public static async Task<bool> fillVacanciesFromPage(ConcurrentBag<VacancyData> bag, string pageAddress)
+        {
+            // проверка на возможность заполнения данных
+            try
+            {
+                IConfiguration config = Configuration.Default.WithDefaultLoader();
+
+                IBrowsingContext context = BrowsingContext.New(config);
+                IDocument document = await context.OpenAsync(pageAddress);
+
+                // Проверка успешного запроса
+                if (document.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    return false;
+                }
+
+                string selector = "div.feed__items > div > article";
+                IHtmlCollection<IElement> articles = document.QuerySelectorAll(selector);
+
+                foreach (var article in articles)
+                {
+                    bag.Add(new VacancyData(
+                        article.QuerySelector("div > div > a > div.flex.align-between > h2").TextContent,
+                        document.Origin + article.QuerySelector("div > div.preview-card__content > a").GetAttribute("href"),
+                        article.QuerySelector("div > div.preview-card__publish > div.publish-info").GetAttribute("title"))
+                    );
+                }
+            }
+            catch (Exception)
+            {
+                return (false);
+            }
+
+            return (true);
+        }
+    }
+
+}
